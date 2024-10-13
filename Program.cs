6bdc88006c2d06d671813e72221075c0c85cf41d
Sarah <Sarah@DESKTOP-M75431O>
2024-10-13 16:38:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Input";
+
+            Console.WriteLine("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + name + "!" );
+            Console.ReadKey();
+        }
+    }
+}
